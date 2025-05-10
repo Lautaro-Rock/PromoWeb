6f7ce8f6df41c3a6cca039564fa7c8c2f86ec4b2
Lautaro Maldonado <lautylty@hotmail.com>
2025-05-10 20:28:44 -0300

    agregando funcionalidad de usuarios

--- Web/WebApplication/register.aspx.cs
+++ Web/WebApplication/register.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
 
 namespace WebApplication
 {
@@ -12,6 +13,41 @@ namespace WebApplication
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void buttonBoucher_Click(object sender, EventArgs e)
+        {
+            string nombre = nombreBox.Text.Trim();
+            string apellido = apellidoBox.Text.Trim();
+            int documento = int.Parse(dniBox.Text);
+            string email= emailBox.Text.Trim();
+            string direccion = direBox.Text.Trim();
+            string ciudad = ciudadBox.Text.Trim();
+            int cpp = int.Parse(cpBox.Text);
+            AccesoDatos accesso = new AccesoDatos();
+            try
+            {
+                accesso.setearConsulta("INSERT INTO Clientes(Documento,Nombre,Apellido,Email,Direccion,Ciudad,CP)"+
+                "VALUES(@Documento,@Nombre,@Apellido,@Email,@Direccion,@Ciudad,@CP);");
+                accesso.setearParametro("@Documento", documento);
+                accesso.setearParametro("@Nombre", nombre);
+                accesso.setearParametro("@Apellido", apellido);
+                accesso.setearParametro("@Email", email);
+                accesso.setearParametro("@Direccion", direccion);
+                accesso.setearParametro("@Ciudad", ciudad);
+                accesso.setearParametro("@CP", cpp);
+                accesso.ejecutarAccion();
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Usuario creado correctamente');", true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                accesso.cerrarConexion();
+            }
         }
     }
 }
